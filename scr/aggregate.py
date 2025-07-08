ff97b683415128716367f182a0280bee4bdf1b07
unknown <macaulim.official@gmail.com>
2025-07-08 15:15:13 -0300

    commitando

--- scr/aggregate.py
+++ scr/aggregate.py
@@ -6,20 +6,16 @@ silver_path = f"data/silver/countries_clean_{datetime.today().date()}.parquet"
 gold_dir = "data/gold"
 Path(gold_dir).mkdir(parents=True, exist_ok=True)
 
-# Carrega dados limpos
+
 df = pd.read_parquet(silver_path)
 
-# --------------------------
-# 1. Idiomas mais falados
-# --------------------------
 langs = df.explode("languages")
 langs_count = langs["languages"].value_counts().reset_index()
 langs_count.columns = ["language", "num_countries"]
 langs_count.to_parquet(f"{gold_dir}/languages_most_spoken.parquet", index=False)
 
-# --------------------------
-# 2. Capitais com mais idiomas oficiais
-# --------------------------
+
+
 df_langs = df[["capital", "num_languages"]].dropna()
 df_langs_sorted = df_langs.sort_values(by="num_languages", ascending=False)
 df_langs_sorted.to_parquet(f"{gold_dir}/capitals_by_language_count.parquet", index=False)
--- scr/transform.py
+++ scr/transform.py
@@ -3,19 +3,18 @@ import pandas as pd
 from datetime import datetime
 from pathlib import Path
 
-# Criar diretório 'data/silver' se não existir
 silver_dir = Path("data/silver")
 silver_dir.mkdir(parents=True, exist_ok=True)
 
-# Caminho do arquivo
+
 bronze_path = f"data/bronze/countries_raw_{datetime.today().date()}.json"
 silver_path = silver_dir / f"countries_clean_{datetime.today().date()}.parquet"
 
-# Carregar dados da camada bronze
+
 with open(bronze_path, "r", encoding="utf-8") as f:
     raw_data = json.load(f)
 
-# Limpeza e estruturação
+
 clean_data = []
 for country in raw_data:
     capital = country.get("capital", [None])[0]
@@ -26,7 +25,6 @@ for country in raw_data:
         "num_languages": len(languages)
     })
 
-# Salvar como Parquet na camada silver
 df = pd.DataFrame(clean_data)
 df.to_parquet(silver_path, index=False)
 print(f"[✔] Dados salvos na camada Silver: {silver_path}")
